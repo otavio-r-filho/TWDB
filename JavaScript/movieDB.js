da4737f616a5b1c0e17e00568bb296ca3c20d2fd
Otávio Vicente Ribeiro <otavio.r.filho@gmail.com>
2016-06-23 14:31:19 -0300

    movieDB

---
+++ JavaScript/movieDB.js
@@ -0,0 +1,31 @@
+var movies = [
+	{
+		title: "In Bruges",
+		rating: 5,
+		hasWatched: true
+	},
+	{
+		title: "Frozen",
+		rating: 4.5,
+		hasWatched: false
+	},
+	{
+		title: "Mad Max Fury Road",
+		rating: 5,
+		hasWatched: true
+	},
+	{
+		title: "Les Miserables",
+		rating: 3.5,
+		hasWatched: false
+	}
+]
+
+for(var i = 0; i < movies.length; i++) {
+	var msg = "You have ";
+	if(!movies[i].hasWatched) {
+		msg = msg + "not ";
+	}
+	msg = msg + "watched \"" + movies[i].title + "\" - " + movies[i].rating + " stars";
+	console.log(msg);
+}
