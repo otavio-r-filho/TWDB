0479352be6f0cfa468ffecb5def8bc052a80ffb9
Otávio Vicente Ribeiro <otavio.r.filho@gmail.com>
2016-06-23 12:05:40 -0300

    New exercise. objectSelector

---
+++ JavaScript/objectSelectorEx.js
@@ -0,0 +1,7 @@
+//Select the first <p> tag 
+var tag;
+tag = document.getElementById("first");
+tag = document.getElementByClassName("special");
+tag = document.getElementByTagName("p");
+tag = document.querySelector("#first");
+tag = document.querySelector(".special");
